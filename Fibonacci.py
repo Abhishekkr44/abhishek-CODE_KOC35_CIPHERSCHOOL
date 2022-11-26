354614a04cf1f1489646ecacef966f21c9ab5fb0
Abhishek kr <119110015+Abhishekkr44@users.noreply.github.com>
2022-11-26 21:58:21 +0530

    Update for taking user input

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # python program to check if x is a perfect square
 
 import math
  
 # A utility function that returns true if x is perfect square
-
 def isPerfectSquare(x):
 
     s = int(math.sqrt(x))
@@ -17,6 +10,7 @@ def isPerfectSquare(x):
     return s*s == x
  
 # Returns true if n is a Fibonacci Number, else false
+
 
 def isFibonacci(n):
  
@@ -27,16 +21,22 @@ def isFibonacci(n):
 
     return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4)
 
+
+# taking inputs from user
+numbers = input('Enter numbers (1 2 3...): ') 
+numbers = numbers.split(' ') # splitting with space
+
+
     
 # A utility function to test above functions
 
-for i in range(1,11):
-
-     if (isFibonacci(i) == True):
-
-         print(i,"is a Fibonacci Number") 
-
-     else:
-
-         print(i,"is a not Fibonacci Number ")
-
+for i in numbers:
+    try:
+        i = int(i)
+    except:
+        print(f'{i} is not a valid number.')
+        continue
+    if (isFibonacci(i) == True):
+        print(i,"is a Fibonacci Number") 
+    else:
+        print(i,"is a not Fibonacci Number ")
